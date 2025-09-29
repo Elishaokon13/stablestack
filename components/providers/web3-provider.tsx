4fc679b1951ed7eba7eec601c3588264eb9622c5
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 02:48:11 +0100

    Modified components/providers/web3-provider.tsx

--- components/providers/web3-provider.tsx
+++ components/providers/web3-provider.tsx
@@ -2,7 +2,6 @@
 
 import { SessionProvider } from "next-auth/react";
 import { AppKitProvider } from "@reown/appkit/react";
-import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import appKit from "@/lib/appkit-config";
@@ -17,11 +16,9 @@ export default function Web3Provider({ children }: Web3ProviderProps) {
   return (
     <SessionProvider>
       <AppKitProvider>
-        <WagmiProvider config={appKit.wagmiConfig}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </WagmiProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
       </AppKitProvider>
     </SessionProvider>
   );
