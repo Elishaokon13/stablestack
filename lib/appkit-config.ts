f5b9164589e6cafb57ea665ef1d6155dc62ed86b
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 03:04:48 +0100

    Modified lib/appkit-config.ts

--- lib/appkit-config.ts
+++ lib/appkit-config.ts
@@ -2,6 +2,7 @@ import { createAppKit } from "@reown/appkit/react";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { mainnet, sepolia } from "@reown/appkit/networks";
 import { siweConfig } from "./siwe-config";
+import { cookieStorage, createStorage } from "wagmi";
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
 
@@ -9,9 +10,19 @@ if (!projectId) {
   throw new Error("NEXT_PUBLIC_PROJECT_ID is not set");
 }
 
+// Create Wagmi adapter
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  networks: [mainnet, sepolia],
+  projectId,
+});
+
 // Create the modal
 export const appKit = createAppKit({
-  adapters: [new WagmiAdapter()],
+  adapters: [wagmiAdapter],
   projectId,
   networks: [mainnet, sepolia],
   defaultNetwork: mainnet,
