43ef4b5b0430b356e497b251635e139fc7729bfb
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 23:29:31 +0100

    Modified app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
+import React from "react";  
 import DashboardPageLayout from "@/components/dashboard/layout";
 import DashboardStat from "@/components/dashboard/stat";
-import DashboardChart from "@/components/dashboard/chart";
-import RebelsRanking from "@/components/dashboard/rebels-ranking";
-import SecurityStatus from "@/components/dashboard/security-status";
+import DashboardChart from "../components/dashboard/chart";
+import RebelsRanking from "../components/dashboard/rebels-ranking";
+import SecurityStatus from "../components/dashboard/security-status";
 import BracketsIcon from "@/components/icons/brackets";
 import GearIcon from "@/components/icons/gear";
 import ProcessorIcon from "@/components/icons/proccesor";
