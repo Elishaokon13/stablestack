cc8353421c3d9dd3a930bff672e7e19448d1a32a
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 04:23:33 +0100

    Modified components/providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
+import { AppKitProvider, wagmiAdapter } from "@/lib/appkit-config";
+import { SessionProvider } from "next-auth/react";
+import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -8,7 +11,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+        <SessionProvider>
+          <AppKitProvider>
+            {children}
+          </AppKitProvider>
+        </SessionProvider>
+      </WagmiProvider>
     </QueryClientProvider>
   );
 }
