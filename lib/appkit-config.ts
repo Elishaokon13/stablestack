1258f44d30d1c4e14e1e8b9fdc6ac7367d0396c8
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 04:43:55 +0100

    Modified lib/appkit-config.ts

--- lib/appkit-config.ts
+++ lib/appkit-config.ts
@@ -1,6 +1,6 @@
 import { createAppKit } from "@reown/appkit/react";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { mainnet, baseSepolia } from "@reown/appkit/networks";
+import { base, baseSepolia } from "@reown/appkit/networks";
 import { siweConfig } from "./siwe-config";
 import { cookieStorage, createStorage } from "wagmi";
 
