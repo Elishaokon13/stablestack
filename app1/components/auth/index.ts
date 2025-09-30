56a70c295e1575dfc824985f50ff87f4f56608e4
Elishaokon13 <eliscodavid@gmail.com>
2025-09-30 04:45:33 +0100

    Modified app1/components/auth/index.ts

--- app1/components/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { WalletAuth } from './Wallet'
-export { ReownProvider } from './Reown'
-export { AppKitButton, AppKitButtonCompact, AppKitButtonFull } from './AppKitButton'
-export { WalletStatus } from './WalletStatus'
