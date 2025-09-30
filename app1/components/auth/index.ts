da57dac76e294f00e4bfd236d12fd014cf5ecb1e
Elishaokon13 <eliscodavid@gmail.com>
2025-09-30 04:04:56 +0100

    Modified components/auth/index.ts

--- app1/components/auth/index.ts
+++ app1/components/auth/index.ts
@@ -1,3 +1,4 @@
 export { WalletAuth } from './Wallet'
 export { ReownProvider } from './Reown'
 export { AppKitButton, AppKitButtonCompact, AppKitButtonFull } from './AppKitButton'
+export { WalletStatus } from './WalletStatus'
